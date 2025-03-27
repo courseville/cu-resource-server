fd5a9c5daa7c0b2c39974542a95a0e767640b281
ChawinNg <ik.chawin@gmail.com>
2025-03-27 15:57:50 +0700

    feat: some mock api

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
+use Laravel\Passport\Client;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
+})->middleware('auth:api');
+
+Route::get('/test-mock', function (Request $request) {
+    return Client::get();
 })->middleware('auth:api');
