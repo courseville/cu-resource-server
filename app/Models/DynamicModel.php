f74a94bae1c4e6d543baeffab7bc1dace443ae3e
ChawinNg <ik.chawin@gmail.com>
2025-03-29 18:57:20 +0700

    feat: dynamic routing api with passport scope check

---
+++ app/Models/DynamicModel.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class DynamicModel extends Model
+{
+    protected $guarded = [];
+
+    public function setTableName($table)
+    {
+        $this->setTable($table);
+        return $this;
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\DynamicModel;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\User;
@@ -12,7 +14,7 @@ Route::get('/user', function (Request $request) {
 
 Route::get('/users', function (Request $request) {
     return User::get();
-})->middleware(['auth:api','scopes:admin,student']);
+})->middleware(['auth:api', 'scopes:admin,student']);
 
 Route::get('/test-mock', function (Request $request) {
     return Client::get();
@@ -20,11 +22,28 @@ Route::get('/test-mock', function (Request $request) {
 
 Route::get('/user-withscope', function (Request $request) {
     return $request->user();
-})->middleware(['auth:api','scope:student']);
+})->middleware(['auth:api', 'scope:student']);
 
 Route::get('/scopes', function (Request $request) {
-    $scopes = Passport::scopes(); 
-    
-    return response()->json($scopes); 
+    $scopes = Passport::scopes();
+
+    return response()->json($scopes);
 })->middleware('client:machine');
+
+Route::middleware('auth:api')->get('/resources/{entity}', function (Request $request, $entity) {
+    if (!Schema::hasTable($entity)) {
+        abort(404, "Table not found");
+    }
+
+    $scopes = [
+        'users' => 'admin',
+    ];
+
+    if (isset($scopes[$entity]) && !$request->user()->tokenCan($scopes[$entity])) {
+        abort(403, "Unauthorized: Missing scope {$scopes[$entity]}");
+    }
+
+    $model = (new DynamicModel())->setTableName($entity);
+    return response()->json($model->get());
+});
 
