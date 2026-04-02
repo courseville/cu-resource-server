c34270686bc0a7f8c483fb34a44d5cb813141116
Lattapon Jeerapradit <lattapon.jeerapradit@gmail.com>
2026-04-02 16:39:40 +0700

    Update AdminPanelProvider.php

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -28,8 +28,7 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('admin')
-            ->login()
-            // ->login(Login::class)
+            ->login(Login::class)
             ->authGuard('admin')
             ->colors([
                 'primary' => Color::Amber,
