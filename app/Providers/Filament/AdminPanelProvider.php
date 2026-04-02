ab962f341426746a72b43be6c1143f77dba7c35f
Lattapon Jeerapradit <lattapon.jeerapradit@gmail.com>
2026-04-02 13:12:04 +0700

    Revert "login frontend email validation removed"
    This reverts commit ddab965c7112737b357c257e6010429a5ba52ed0.

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -27,7 +27,7 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('admin')
-            ->login(\App\Filament\Pages\Auth\Login::class)
+            ->login()
             ->authGuard('admin')
             ->colors([
                 'primary' => Color::Amber,
